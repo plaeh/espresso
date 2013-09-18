0df4effc7c9336441bf685d7cf02476a9d3189bd
Alexia McKenzie <pandapprentice@gmail.com>
2013-09-18 01:10:50 -0700

    working slot machine, button, and winning mechanism

--- js/slotmachine.js
+++ js/slotmachine.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
 var IMAGE_HEIGHT = 150;
 var DURATION = 10000; 
 var ITEM_COUNT = 3; 
@@ -17,36 +16,29 @@ var results = new Array();
     results[7] = "Tasty!";
     results[8] = "Tea Time!";
 
-
-
 $play.click(function(){
     var spin_value = Math.floor(Math.random()*ITEM_COUNT)
 
-    goToPosition('machine', spin_value)
-    goToPosition('filter', spin_value)
-    goToPosition('material', spin_value)
+    var display_results = _.after(ITEM_COUNT, function() {
+        var random_result_text = results[Math.floor(Math.random() * results.length)]
+        $('#outcome .message').text(random_result_text)
+    })
+
+    goToPosition('machine', spin_value, display_results)
+    goToPosition('filter', spin_value, display_results)
+    goToPosition('material', spin_value, display_results)
 })
 
-
-function goToPosition(slot_type, position) {
+function goToPosition(slot_type, position, callback) {
     var slot = $('.slot#' + slot_type)
     var offset = ( ITEM_COUNT + position ) * IMAGE_HEIGHT
 
 
-    slot.css({ 
-        'background-position-y': '0px'
-     })
+    slot.css({ 'background-position-y': '0px' })
 
 
-    slot.animate({ 
-        'background-position-y': offset + 'px' 
-    },
-    {
-        complete: function(){
-            var random_result_text = results[Math.floor(Math.random() * results.length)]
-            alert(random_result_text);
-        }
+    slot.animate({ 'background-position-y': offset + 'px' }, function() {
+        callback()
     })
 }
-
 })
