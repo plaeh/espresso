48e4c8f168e16e04ca675c55b0aaa796db87e9ed
Alexia McKenzie <pandapprentice@gmail.com>
2013-09-17 22:07:25 -0700

    headers; spans; slotmachine.js- click event, random results, animate;

--- js/slotmachine.js
+++ js/slotmachine.js
@@ -2,28 +2,56 @@ $(document).ready(function() {
 
 
 var IMAGE_HEIGHT = 150;
-var DURATION = 1000; 
+var DURATION = 10000; 
 var ITEM_COUNT = 3; 
 var RANDOM = Math.floor(Math.random()*3);
-var $s1= $('machine')
-var $s2 = $('filter')
-var $s3 = $('material')
-var $play = $('.play')
+var $play = $('#play')
+
+var results = new Array();
+    results[0] = "Enjoy";
+    results[1] = "Coffee Time!";
+    results[2] = "Drink up!";
+    results[3] = "Yum!";
+    results[4] = "Coffee~";
+    results[5] = "Tea";
+    results[6] = "Espresso!";
+    results[7] = "Tasty!";
+    results[8] = "Tea Time!";
+
+
+$play.click(function(){
+    goToPosition('machine', 300)
+    goToPosition('filter', 300)
+    goToPosition('material', 300)
+    var random_result_text = results[Math.floor(Math.random() * results.length)]
+    _.after(alert(random_result_text));
+
+})
+
 
 function goToPosition(slot_type, position) {
     var slot = $('.slot#' + slot_type)
     var offset = position * IMAGE_HEIGHT
 
+
+    slot.css({ 
+        'background-position-y': '0px'
+     })
+
+
     slot.animate({ 
         'background-position-y': offset + 'px' 
     },
     {
-        easing: 'easeOut',
-        duration: DURATION,
         complete: function(){
-            alert('Enjoy!');
+            var random_result_text = results[Math.floor(Math.random() * results.length)]
+            alert(random_result_text);
         }
     })
 }
+
+var random_result_text = results[Math.floor(Math.random() * results.length)]
+_.after(alert(random_result_text));
+
 
 })
